7fd0e96f51fd4bccfd7ff2802a0b80a90bd8d4e1
Anna <koshlakova_av@mail.ru>
2017-03-11 18:57:09 +0300

    Добавить бота

---
+++ bot.py
@@ -0,0 +1,28 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from config import api_token
+
+def main():
+	updater = Updater(api_token)
+
+	dp = updater.dispatcher
+	dp.add_handler(CommandHandler('start', greet_user))
+	dp.add_handler(MessageHandler([Filters.text], talk_to_me))
+
+	dp.add_error_handler(show_error)
+
+	updater.start_polling()
+	updater.idle()
+
+def greet_user(bot, update):
+	print('AAA')
+	print(update)
+	bot.sendMessage(update.message.chat_id, text = 'Давай общаться')
+
+def show_error(bot, update, error):
+	print(error)
+
+def talk_to_me(bot, update):
+	print(update.message.text)
+	bot.sendMessage(update.message.chat_id, update.message.text)
+
+main()
