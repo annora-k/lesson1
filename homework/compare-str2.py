207f5b88e9496944c139f2f17b98410a411204b1
Anna Koshlakova <a.koshlakova@dwteam.ru>
2017-03-17 12:50:22 +0300

    Файлы ДЗ

---
+++ homework/compare-str2.py
@@ -0,0 +1,22 @@
+def get_text(text1, text2):
+	a = None
+
+	if text1 == text2:
+		a  = 1
+
+	elif text1 != text2 and len(text1) > len(text2):
+		a = 2
+
+
+	elif  text1 != text2 and text2 == 'learn':
+		a = 3
+
+	return a
+
+print('Привет')
+
+text1 = input('Введите строку 1: ')
+text2 = input('Введите строку 2: ')
+
+result = get_text(text1, text2)
+print(result)
