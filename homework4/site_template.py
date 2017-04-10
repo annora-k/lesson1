b2a6eccf81e8480bdec5fff115da495ecaf59652
Anna Koshlakova <a.koshlakova@dwteam.ru>
2017-04-10 16:25:09 +0300

    ДЗ-4

--- homework4/site_template.py
+++ homework4/site_template.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 my_flask_app = Flask(__name__)
 
 
-@my_flask_app.route('/')
+@my_flask_app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 if __name__ == "__main__":
     my_flask_app.run(port=5001, debug=True)
