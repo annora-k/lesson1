9e9223ca7f568a04378d924b2dd682728e6a1841
Anna Koshlakova <a.koshlakova@dwteam.ru>
2017-04-10 11:12:11 +0300

    homework4

--- db.py
+++ db.py
@@ -10,8 +10,9 @@ db_session = scoped_session(sessionmaker(bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
+
 class User(Base):
-    __tablename__ == 'users'
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     first_name = Column(String(50))
     last_name = Column(String(50))
@@ -25,5 +26,5 @@ class User(Base):
     def __repr__(self):
         return '<User {} {}'.format(self.first_name, self.last_name)
 
-if __name__ == "_main__":
+if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
--- metro.py
+++ metro.py
@@ -1,5 +1,5 @@
 import csv
-from datetime import datetime
+
 
 with open('moscow_metro_info.csv', 'r', encoding='utf-8') as f:
 	
@@ -10,13 +10,7 @@ with open('moscow_metro_info.csv', 'r', encoding='utf-8') as f:
 		if row['Ремонт эскалаторов']:
 			repair.append(row['Станция метрополитена'])
 
-			date_today = datetime.now()
-			date = row['Ремонт эскалаторов']split('-')
-			date_1 = date[0]
-			date_2 = date[1]
-
-			if row['Ремонт эскалаторов']
-
+			
 
 
 print(set(repair))
